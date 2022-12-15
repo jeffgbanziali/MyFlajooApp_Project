48d5b707bf1df0503affe041722d62df6cf242bc
Jeff Gbanziali <jeff.gbanziali@efrei.net>
2022-12-15 15:43:43 +0100

    first commit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text} >Bonjour </Text>
+      
     </View>
   );
 }
@@ -13,8 +13,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#2C2828',
+    textColor: 'white',
   },
+  text: {
+    color: 'white',
+  }
 });
