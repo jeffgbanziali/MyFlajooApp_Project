e0f955ce51ba48d56154b2b66dec861ff850cd97
Jeff Gbanziali <jeff.gbanziali@efrei.net>
2022-12-17 04:05:16 +0100

    first commit

--- screens/SignInScreen/SignInScreen.js
+++ screens/SignInScreen/SignInScreen.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { ImageBackground } from 'react-native';
+
+
+
 
 const SignInScreen = () => {
     return (
         <>
-           
-                <View style={{ alignItems: "center", width: 460 }}>
+            <ImageBackground source={require("../../assets/Images/Background.jpg")}>
+                <View style={{ alignItems: "center", width: 460, background: "blue", }}>
                     <Text style={{ color: 'white', fontSize: 64, fontWeight: 'bold', marginVertical: 10 }}>
                         Login
                     </Text>
                     <View style={{ backgroundColor: 'white', height: 700, width: 460, borderTopLeftRadius: 160, paddingTop: 100 }}>
-<Text>My Name</Text>
+                        <Text>My Name</Text>
 
                     </View>
                 </View>
-        
+            </ImageBackground>
+
+
 
 
         </>
