955a48207d0c2c1c691563ffa6b1c4b1b973d1c3
Jeff Gbanziali <jeff.gbanziali@efrei.net>
2022-12-17 02:14:06 +0100

    first commit

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
-
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen/HomeScreen';
-import SignInScreen from './screens/SignInScreen/SignInScreen';
-import SignUpScreen from './screens/SignUpScreen/SignUpScreen';
-
-
-
+import { View } from 'react-native';
 
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="SignIn" component={SignInScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.root}>
+      <Navigation />
+    </View>
   );
-}
+};
 
 export default App;
--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -7,20 +7,24 @@ import { darkRose } from '../../components/Button/Constants';
 
 function HomeScreen(props) {
     return (
+
         <>
+
             <View style={{ marginHorizontal: 40, marginVertical: 100 }}>
                 <Text style={{ color: "black", fontSize: 64 }}>Let's Start </Text>
                 <Text style={{ color: "black", fontSize: 64, marginBottom: 40 }}>Welcome </Text>
-                <Button bgColor={darkRose} textColor="white" buttonLabel="Continue with Email" Press={() => props.navigation.navigate("SignUp")} />
+                <Button bgColor={darkRose} textColor="white" buttonLabel="Continue with Email" Press={() => props.navigation.navigate("Signup")} />
                 <Button bgColor={darkRose} textColor='white' buttonLabel="Continue with Facebook" Press={() => props.navigation.navigate("loginFacebook")} />
                 <Button bgColor={darkRose} textColor='white' buttonLabel="Continue with Google" Press={() => props.navigation.navigate("loginGoogle")} />
             </View>
             <View style={{ marginHorizontal: 40, marginVertical: -38, }}>
-                <Text style={{marginHorizontal: 40, color: "black", fontSize: 20, display: 'flex' }}>Have an account ?
+                <Text style={{ marginHorizontal: 40, color: "black", fontSize: 20, display: 'flex' }}>Have an account ?
                 </Text>
-                <Button bgColor='#1565C0' buttonLabel='Sign In' Press={() => props.navigation.navigate("SignIn")} />
+                <Button bgColor='#1565C0' buttonLabel='Sign In' Press={() => props.navigation.navigate("Signin")} />
 
             </View>
+
+
 
 
         </>
