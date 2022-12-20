4edd5e1c90a16b683cd58aa7e47f427ac9ce081a
Jeff Gbanziali <jeff.gbanziali@efrei.net>
2022-12-20 02:51:00 +0100

    commit soon

--- Data/MyPostUser.js
+++ Data/MyPostUser.js
@@ -45,6 +45,72 @@ export const POSTS = [
                 comment: 'This is a comment',
             },
         ]
+    },
+    {
+        imageUrl: "https://static1.purepeople.com/articles/6/29/96/06/@/4239650-salma-hayek-decembre-2017-624x600-3.jpg",
+        user: USER[0].user,
+        time: '10 min ago',
+        likes: '1,230',
+        caption: 'This is a caption',
+        profilePicture: USER[0].image,
+        comments: [
+            {
+                user: "koukouda",
+                comment: 'This is a comment',
+            },
+            {
+                user: "koukouda",
+                comment: 'This is a comment',
+            },
+            {
+                user: "eff Flaj",
+                comment: 'This is a comment',
+            },
+        ]
+    },
+    {
+        imageUrl: 'https://www.francesoir.fr/sites/default/files/styles/max_1300x1300/public/2016-08/salma-hayek-francesoir.jpg?itok=iCpunysi',
+        user: USER[0].user,
+        time: '10 min ago',
+        likes: '1,230',
+        caption: 'This is a caption',
+        profilePicture: USER[0].image,
+        comments: [
+            {
+                user: "koukouda",
+                comment: 'This is a comment',
+            },
+            {
+                user: "koukouda",
+                comment: 'This is a comment',
+            },
+            {
+                user: "eff Flaj",
+                comment: 'This is a comment',
+            },
+        ]
+    },
+    {
+        imageUrl: 'https://image.stern.de/31771652/t/eh/v2/w1440/r1.7778/-/10--das-sind-ihre-beauty--und-mode-favoriten---16-9---spoton-article-1019904.jpg',
+        user: USER[0].user,
+        time: '10 min ago',
+        likes: '1,230',
+        caption: 'This is a caption',
+        profilePicture: USER[0].image,
+        comments: [
+            {
+                user: "koukouda",
+                comment: 'This is a comment',
+            },
+            {
+                user: "koukouda",
+                comment: 'This is a comment',
+            },
+            {
+                user: "eff Flaj",
+                comment: 'This is a comment',
+            },
+        ]
     }
 
 ]
