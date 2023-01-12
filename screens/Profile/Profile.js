996021d9119dc3499c64f64b45dc98683c1c5e5e
Jeff Gbanziali <jeff.gbanziali@efrei.net>
2023-01-12 22:00:48 +0100

    My project works wonderfully

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -3,31 +3,81 @@ import React, { } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { Avatar } from 'react-native-elements';
 import { Button } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 // create a component
 const Profile = () => {
     return (
         <ScrollView style={styles.container}>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Avatar
-                    rounded
-                    size="large"
-                    source={{
-                        uri:
-                            'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-                    }}
-                />
-                <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>
-                    John Doe
-                </Text>
-                <Text style={{ fontSize: 15, color: 'gray', marginTop: 5 }}>
-                    Full Stack Developer
-                </Text>
-                <Button
-                    title="Edit Profile"
-                    buttonStyle={{ marginTop: 20, backgroundColor: '#4388d6' }}
-                />
+            <View
+                style={{
+                    backgroundColor: '#3D3939',
+                    borderRadius: 30,
+                    paddingBottom: 90,
+                    marginTop: 50,
+
+                }}>
+                <View
+                    style={{
+                        flex: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        marginTop: 18,
+                    }}>
+                    <TouchableOpacity>
+                        <Avatar
+                            size="xlarge"
+                            rounded
+                            source={{
+                                uri:
+                                    'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                            }} />
+
+                    </TouchableOpacity>
+
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>
+                        John Doe
+                    </Text>
+                    <Text style={{ fontSize: 15, color: 'gray', marginTop: 5 }}>
+                        Full Stack Developer
+                    </Text>
+
+                </View>
+
             </View>
+            <View style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <View style={{
+                    width: 371,
+                    height: 84,
+                    backgroundColor: '#494747',
+                    borderRadius: 30,
+                    paddingBottom: 90,
+                    marginTop: -50,
+                    borderWidth: 1,
+                    borderColor: '#EFEAEA',
+                    borderStyle: 'solid',
+
+                }}>
+                    <View>
+                        <Text style={{
+                            color: 'white',
+                            fontSize: 20,
+                            fontWeight: 'bold',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            textAlign: 'center',
+
+                        }}>
+                            Bonjour
+                        </Text>
+                    </View>
+                </View>
+            </View>
+
+
         </ScrollView>
 
     );
